f8a50710a3e872f89957161fc0c3546a80f8c56f
cromiium <yskilitis@gmail.com>
2018-10-20 01:41:41 -0400

    Mainly a test but also added comments to function prototypes

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include "Book.h"
 
-void init();
-void save();
-void display();
-void addBook();
-void alterBook();
-void deleteBook();
+void init(); //Initializes application
+void save(); //Saves application
+void display(); //Writes what's in file on screen
+void addBook(); //Writes a book to file
+void alterBook(); //Changes book in a file
+void deleteBook(); //Deletes book from file
 
 int main(){
 
